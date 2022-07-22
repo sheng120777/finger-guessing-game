072f787a41b9d272ed3ec0a7cd9a021d7547dcd4
sheng <86102390+sheng120777@users.noreply.github.com>
2022-07-22 19:34:56 +0800

    Update finger_guessing.py

--- finger_guessing.py
+++ finger_guessing.py
@@ -8,10 +8,11 @@ from cvzone.HandTrackingModule import HandDetector
 from cvzone.PoseModule import PoseDetector
 from cvzone.SerialModule import SerialObject
 import time
+import mediapipe
 
 cap=cv2.VideoCapture(0)
 detector=HandDetector(detectionCon=0.5,maxHands=1) 
-myserial=SerialObject("COM8",9600,1)         #com port 可調 
+myserial=SerialObject("COM8",9600,1)
 
 while True:
     success,img=cap.read()
@@ -19,10 +20,22 @@ while True:
     lmList,bbox=detector.findPosition(img)
     if lmList:
         fingers=detector.fingersUp()
-        print(fingers)   
-        myserial.sendData(fingers)
+        #print(fingers)   
+        #myserial.sendData(fingers)
+        if fingers==[1,1,1,1,1]:
+            print("paper")
+            out=[0,0,1,1,0]      
+            myserial.sendData(out)             
+        if fingers==[0,1,1,0,0]:
+            print("scissor")
+            out=[0,0,0,0,0]
+            myserial.sendData(out)
+        if fingers==[0,0,0,0,0]:
+            print("rock")
+            out=[1,1,1,1,1]
+            myserial.sendData(out)
     img=img[:,::-1]
     cv2.imshow('img',img)
-    if cv2.waitKey(1)==ord('q'):           #按Q退出
+    if cv2.waitKey(1)==ord('q'):
         break
 cv2.destroyAllWindows() 
